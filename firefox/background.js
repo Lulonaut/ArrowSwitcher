5f5867f5d62a2c8c0732c73751007f97133c021e
Lulonaut <lulonaut@tutanota.de>
2022-05-07 21:52:55 +0200

    Firefox version

---
+++ firefox/background.js
@@ -0,0 +1,23 @@
+async function changeTab(amount) {
+  await browser.tabs.query({ currentWindow: true }, (tabs) => {
+    for (let i = 0; i < tabs.length; i++) {
+      let tab = tabs[i];
+      if (tab.active) {
+        if (i + amount >= 0 && i + amount < tabs.length) {
+          let newTabId = tabs[i + amount].id;
+          browser.tabs.update(newTabId, { highlighted: true });
+          browser.tabs.update(tab.id, { highlighted: false });
+        }
+      }
+    }
+  });
+}
+
+browser.commands.onCommand.addListener((command) => {
+  console.log(command);
+  if (command === "tab-left") {
+    changeTab(-1);
+  } else if (command === "tab-right") {
+    changeTab(1);
+  }
+});
